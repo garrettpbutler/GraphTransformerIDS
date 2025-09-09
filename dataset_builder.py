3cbd53e60bd7cb416f8d3bfadd129c84dcf047f3
Garrett Butler <gpb00001@mail.wvu.edu>
2025-09-09 12:51:33 -0400

    Bug fixes for prototype

--- dataset_builder.py
+++ dataset_builder.py
@@ -3,10 +3,11 @@ import torch
 from torch_geometric.data import Data
 
 def build_graph_from_csv(csv_path: str = None, df: pd.DataFrame = None):
-    if df is None and csv_path is not None:
-        df = pd.read_csv(csv_path)
-    elif df is None:
-        raise ValueError("Eitehr csv_path or df must be provided")
+    if df is None:
+        if csv_path is not None:
+            df = pd.read_csv(csv_path)
+        else:
+            raise ValueError("Eitehr csv_path or df must be provided")
 
     node_map = {}  # maps string ID -> node index
     edges_src = []
